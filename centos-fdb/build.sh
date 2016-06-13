b5ea52c9b29cd442f65ec7d9a5dedeccd5e2de94
George Sapkin <george.sapkin@gmail.com>
2016-06-13 02:58:09 +0200

    Removed centos-fdb

--- centos-fdb/build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-docker build -t georgesapkin/centos-fdb .
-#docker tag $id georgesapkin/centos-fdb:2.0.5
-#docker tag $id georgesapkin/centos-fdb:latest
--- centos-fdb/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Erasing older cluster information file from /etc/foundationdb/fdb.cluster"
-sudo rm /etc/foundationdb/fdb.cluster
-docker run -d -v /etc/foundationdb:/etc/foundationdb:rw -P georgesapkin/centos-fdb supervisord -c /etc/supervisord.conf
